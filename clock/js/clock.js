32885c88bfa72a8befc1849c1984d06994f363e8
cbolson <chris@cbolson.com>
2022-11-10 21:58:51 +0100

    add clock numbers via JS

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -4,18 +4,27 @@ Date : 2022-11-09
 Changes: 
 - separate large function into smaller functions
 - move hour hand to position between numbers to reflect minutes (dev)
+- place 12 numbers around the clock face (could be a var to define which we want to show)
 */
+
 // place numbers around clock face
 const elNumbers = document.querySelectorAll(".numbers li");
+function placeNumbers() {
+  /*
+next step is to add li elements dynamically
+*/
 
-elNumbers.forEach((number, index) => {
-  // calculate degrees
-  const degrees = (360 / 12) * index;
-  console.log(degrees);
-
-  number.style.transform = `rotate(${degrees}deg) translate(100px)`;
-  //transform:rotate(46deg) translate(251px) rotate(-46deg);
-});
+  elNumbers.forEach((number, index) => {
+    // calculate degrees
+    const degrees = (360 / 12) * index + 300; // adding 300 to avoid double negatives
+    console.log(`${index} -${degrees}`);
+    // rotate each number
+    number.style.transform = `
+      rotate(${degrees}deg) 
+      translate(120px) 
+      rotate(-${degrees}deg)`;
+  });
+}
 
 const elHour = document.querySelector(".hand-hour");
 const elMin = document.querySelector(".hand-min");
@@ -42,4 +51,6 @@ function updateClock() {
 function moveHand(el, newDeg) {
   el.style.rotate = `${newDeg}deg`;
 }
+
+placeNumbers();
 setInterval(updateClock, 1000);
