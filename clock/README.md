213ce1644eda0db94432b130b441a1207a1e4885
cbolson <chris@cbolson.com>
2022-11-11 11:37:30 +0100

    html title added, readme updated, JS clean up git push origin main clear esc

--- clock/README.md
+++ clock/README.md
@@ -1,13 +1,22 @@
 # HTML + CSS + JavaScript clock
 
+Date : 2022-11-09
+Date mod : 2022-11-10
 Source: https://www.youtube.com/watch?v=DzSLUdJWrEQ&t=6999s
 
-Date : 2022-11-09
+Description:
 
-Date mod : 2022-11-10
+- CSS - clock face and hands
+- Javascript: Numbers created and placed
+- JavaScript: Clock hands positioned every second in realtime
+- JavaScript: Hour hand placed in real position between hours
+- HTML data attribute allows us to define the click numbers to show all the numbers or just 3,6,9,12
 
+Notes:
 Changes made to original source:
 
 - separate large function into smaller functions
-- move hour hand to position between numbers to reflect minutes (dev)
-- place 12 numbers around the clock face (could be a var to define which we want to show)
+- move hour hand to position between numbers to reflect actual minutes
+- place numbers around the clock face via JavaScript:
+  : data-show="all" = show all numbers
+  : data-show="simple" - show only 3,6,9,12
--- clock/js/clock.js
+++ clock/js/clock.js
@@ -3,49 +3,60 @@ Original Source: https://www.youtube.com/watch?v=DzSLUdJWrEQ&t=6999s
 Date : 2022-11-09
 Changes: 
 - separate large function into smaller functions
-- move hour hand to position between numbers to reflect minutes (dev)
-- place 12 numbers around the clock face (could be a var to define which we want to show)
+- move hour hand to position between numbers to reflect actual minutes
+- place numbers around the clock face:
+  : data-show="all" = show all numbers
+  : data-show="simple" - show only 3,6,9,12
 */
 
 // const elNumbers = document.querySelectorAll(".numbers li");
-const elNumbers = document.querySelector(".numbers ol");
+const elNumbers = document.querySelector(".numbers ul");
 const elHour = document.querySelector(".hand-hour");
 const elMin = document.querySelector(".hand-min");
 const elSec = document.querySelector(".hand-sec");
 
-// // place numbers around clock face
-// function placeNumbers() {
-//   /*
-// next step is to add li elements dynamically
-// */
-
-//   elNumbers.forEach((number, index) => {
-//     // calculate degrees
-//     const degrees = (360 / 12) * index + 300; // adding 300 to avoid double negatives
-//     // console.log(`${index} -${degrees}`);
-//     // rotate each number
-//     number.style.transform = `otate(${degrees}deg) translate(120px)  rotate(-${degrees}deg)`;
-//   });
-// }
-
 // function add numbers dynamically
 function addNumbers() {
+  // get "show" attribute to know if we want "all" the numbers or just the "main" numbers
+  const numToShow = elNumbers.dataset.show;
+
+  // define array of main numbers to show
+  const mainNumbers = [3, 6, 9, 12];
+
   for (let i = 1; i <= 12; i++) {
     // create new number li (number is added via CSS counter)
     const elNum = document.createElement("li");
-    // Create a new text node
-    const text = document.createTextNode(`${i}`);
-elNum.appendChild(text);
 
-    // calculate degrees
-    const degrees = (360 / 12) * i + 270; // adding 270 to avoid double negatives
-    // console.log(`${index} -${degrees}`);
-    // rotate each number
-    elNum.style.transform = `rotate(${degrees}deg) translate(120px)  rotate(-${degrees}deg)`;
-    console.log(elNum);
-    elNumbers.appendChild(elNum);
-  }
+    // default show number
+    let showNum = true;
+
+    // if we only want the "main" numbers
+    if (numToShow === "simple" && !mainNumbers.includes(i)) {
+      showNum = false;
+    }
+
+    // add number to clock if showNum=true
+    if (showNum) {
+      // Create a new text node
+      const text = document.createTextNode(`${i}`);
+
+      // add text to new element
+      elNum.appendChild(text);
+
+      // calculate degrees
+      const degrees = calcDegrees(i);
+
+      // rotate each number relative to it's value
+      elNum.style.transform = `rotate(${degrees}deg) translate(120px)  rotate(-${degrees}deg)`;
+
+      // add new element to parent
+      elNumbers.appendChild(elNum);
+    }
+  } // end - for numbers
 }
+
+// calculate degrees (single line arrow function just to try it out)
+const calcDegrees = (i, offset = 270) => (360 / 12) * i + offset; // adding offset to avoid double negatives
 
 // update clock every second with current time
 function updateClock() {
@@ -57,19 +68,22 @@ function updateClock() {
   const curMin = currentDate.getMinutes();
   const curSec = currentDate.getSeconds();
 
-  const degMin = (curMin / 60) * 360; // minute position
-  const degSec = (curSec / 60) * 360; // seconds position
+  // calculate degrees for position of each clock hand
+  const degMin = (curMin / 60) * 360;
+  const degSec = (curSec / 60) * 360;
   const degHour = (curHour * 360) / 12 + degMin / 12; // hour position to be between numbers according to minutes
 
+  // position each clock hand
   moveHand(elHour, degHour);
   moveHand(elMin, degMin);
   moveHand(elSec, degSec);
 }
 
-function moveHand(el, newDeg) {
-  el.style.rotate = `${newDeg}deg`;
-}
+// move clock hand to defined position
+const moveHand = (el, newDeg) => (el.style.rotate = `${newDeg}deg`);
 
+// add numbers
 addNumbers();
-//placeNumbers();
+
+// set time (call every 1 second)
 setInterval(updateClock, 1000);
