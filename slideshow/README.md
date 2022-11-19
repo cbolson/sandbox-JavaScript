f667d11f49a124c5f1c6731c88477623c72ed41d
cbolson <chris@cbolson.com>
2022-11-19 14:18:23 +0100

    JS update to allow mutliple slideshows on one page JS adapted to use data attributes

---
+++ slideshow/README.md
@@ -0,0 +1,15 @@
+# SLideshow
+
+## Description
+
+Simple CSS & JavaScript Slideshow
+
+## Features
+
+[ ] Multiple slideshows on same page
+[ ] Responsive (TO DO)
+
+## Resources
+
+- Images from Lorem Picsum : https://picsum.photos/
+-
--- slideshow/js/slideshow.js
+++ slideshow/js/slideshow.js
@@ -1,25 +1,35 @@
-const slider = document.querySelector("#slider");
-const sliderEl = slider.querySelector("#slider-images");
-const sliderImages = sliderEl.querySelectorAll("img");
-const btPrev = slider.querySelector("#btn-prev");
-const btNext = slider.querySelector("#btn-next");
+// get array of all the slideshows on the page
+// Note - I use the data attribute rather than the css so that if we change the css names nothing breaks
+const sliders = Array.from(document.querySelectorAll("[data-slider]"));
 
-let currentImg = 0;
+sliders.forEach((slider) => {
+  // define each slider vars and functiosn
+  const sliderElement = slider.querySelector("#slider-images");
+  const sliderImages = sliderElement.querySelectorAll("img");
+  const sliderButtons = slider.querySelectorAll("[slider-btn]");
+  let currentImg = 0;
 
-btPrev.addEventListener("click", () => {
-  currentImg--;
-  updateImg();
-});
-btNext.addEventListener("click", () => {
-  currentImg++;
-  updateImg();
-});
+  // add click event to buttons to slide image container
+  sliderButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const direction = btn.getAttribute("slider-btn");
+      slideImg(direction);
+    });
+  });
+  // slide image container
+  function slideImg(direction) {
+    if (direction == "prev") currentImg--;
+    else currentImg++;
 
-function updateImg() {
-  if (currentImg == sliderImages.length) {
-    currentImg = 0;
-  } else if (currentImg < 0) {
-    currentImg = sliderImages.length - 1;
+    if (currentImg == sliderImages.length) {
+      currentImg = 0;
+    } else if (currentImg < 0) {
+      currentImg = sliderImages.length - 1;
+    }
+    // get slider width to be able to calculate transform position
+    // note I am doing this within this function incase the window has been resized after defining the variables
+    const imgW = slider.offsetWidth;
+
+    sliderElement.style.transform = `translateX(-${currentImg * imgW}px)`;
   }
-  sliderEl.style.transform = `translateX(-${currentImg * 600}px)`;
-}
+});
