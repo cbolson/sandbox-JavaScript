ae07d7d66c0a795798666ac46aab60abbdce18d1
cbolson <chris@cbolson.com>
2022-11-20 19:49:40 +0100

    fetch and charting data complete

---
+++ avg-temp/README.md
@@ -0,0 +1,16 @@
+# Average yearly temperature chart
+
+## About
+
+JavaScript Fetch to read world average temperatures from csv file.
+Data is then charted to a line chart.
+
+## Features
+
+- Use chart.js to create a line chart of multiple data.
+
+## Sources
+
+:link: The Coding Train :https://www.youtube.com/watch?v=5-ptp9tRApM
+:link: chart.js : https://www.chartjs.org/
+:link: Nasa https://data.giss.nasa.gov/gistemp/
--- avg-temp/js/script.js
+++ avg-temp/js/script.js
@@ -1,15 +1,64 @@
-getData();
+const ctx = document.getElementById("chart");
+const urlData = "temp-data.csv";
+chartIt();
 
 async function getData() {
-  const response = await fetch("temp-data.csv");
+  const response = await fetch(urlData);
   const data = await response.text();
+  const xDataYears = [];
+  const avgGlobal = [];
+  const avgNorthHem = [];
+  const avgSouthHem = [];
 
   const table = data.split("\n").slice(1);
 
   table.forEach((row) => {
     const cols = row.split(",");
-    const year = cols[0];
-    const temp = cols[1];
-    console.log(year, temp);
+  
+    xDataYears.push(cols[0]);
+
+    avgGlobal.push(parseFloat(cols[1]) + 14);
+    avgNorthHem.push(parseFloat(cols[2]) + 14);
+    avgSouthHem.push(parseFloat(cols[3]) + 14);
+    //
+  });
+  return { xDataYears, avgGlobal, avgNorthHem, avgSouthHem };
+  //console.log(yTemps);
+}
+async function chartIt() {
+  const data = await getData();
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: data.xDataYears,
+      datasets: [
+        {
+          label: "Global",
+          data: data.avgGlobal,
+          borderWidth: 1,
+        },
+        {
+          label: "Northern Hemisphere",
+          data: data.avgNorthHem,
+          borderWidth: 1,
+        },
+        {
+          label: "Southern Hemisphere",
+          data: data.avgSouthHem,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            callback: function (value, index, ticks) {
+              return value + "°";
+            },
+          },
+        },
+      },
+    },
   });
 }
