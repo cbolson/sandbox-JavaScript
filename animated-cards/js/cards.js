f665ea255ce2c4bf2e9513b6ff813256b33823d8
cbolson <chris@cbolson.com>
2022-12-05 13:07:29 +0100

    changes to cards animation

--- animated-cards/js/cards.js
+++ animated-cards/js/cards.js
@@ -1,28 +1,22 @@
 let activeIndex = 0;
 
-const btnLove = document.querySelector("#btn-love");
-const btnHate = document.querySelector("#btn-hate");
+const btnNext = document.querySelector("#btn-next");
+const btnPrev = document.querySelector("#btn-last");
 const groups = document.querySelectorAll(".card-group");
 
-btnLove.addEventListener("click", () => {
+btnNext.addEventListener("click", () => {
   const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
     nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
   currentGroup.dataset.status = "after";
-  nextGroup.dataset.status = "becoming-active-from-before";
-  setTimeout(() => {
-    nextGroup.dataset.status = "active";
-    activeIndex = nextIndex;
-  });
+  nextGroup.dataset.status = "active";
+  activeIndex = nextIndex;
 });
-btnHate.addEventListener("click", () => {
+btnPrev.addEventListener("click", () => {
   const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
     nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
   currentGroup.dataset.status = "before";
-  nextGroup.dataset.status = "becoming-active-from-after";
-  setTimeout(() => {
-    nextGroup.dataset.status = "active";
-    activeIndex = nextIndex;
-  });
+  nextGroup.dataset.status = "active";
+  activeIndex = nextIndex;
 });
