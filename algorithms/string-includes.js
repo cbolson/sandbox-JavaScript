6f5d1c090f606703e78262749726c05c0b7187a6
cbolson <chris@cbolson.com>
2023-02-07 09:53:34 +0100

    update string-includes.js added variations on base function

--- algorithms/string-includes.js
+++ algorithms/string-includes.js
@@ -2,15 +2,21 @@
 wite a function that takes 2 strings, A & B
 and returns whether B is in A (case insensitive)
 */
-function stringIncludes(str1, str2) {
-  // let strTemp = str1.toLowerCase();
-
-  return str1.toLowerCase().includes(str2.toLowerCase());
-
-  // const arr1 = str1.toLowerCase().split(" ");
-  // if (arr1.includes(str2.toLowerCase())) return true;
-  // return false;
+function incExact(str1, str2) {
+  // if we want exact match including case
+  return str1.split(" ").includes(str2);
 }
+function incIgnoreCase(str1, str2) {
+  // exists anywhere in string ignoring case
+  return str1.toLowerCase().includes(str2.toLowerCase());
+}
+function incIgnoreCaseExact(str1, str2) {
+  // if we need exact word match we need to split string into array
+  return str1.toLowerCase().split(" ").includes(str2.toLowerCase());
+}
+const str1 = "I like cats";
+const str2 = "Cat";
 
-console.log(stringIncludes("I am happy", "Am"));
-console.log(stringIncludes("I like cats", "dog"));
+console.log(incExact(str1, str2)); // expect "false"
+console.log(incIgnoreCase(str1, str2)); // expect "true"
+console.log(incIgnoreCaseExact(str1, str2)); // expect "false"
