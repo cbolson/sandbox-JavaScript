c291509da1dad055d2ce2c79da759e209c7f52a4
cbolson <chris@cbolson.com>
2023-02-08 22:08:50 +0100

    modify - numbers-sum.js Separate function to show 2 methods using loop and reduce

--- algorithms/numbers-sum.js
+++ algorithms/numbers-sum.js
@@ -2,14 +2,15 @@
 write a function that takes any number of arguments of type number
 and returns the sum of all the numbers
 */
-function sum(...arr) {
+function sumLoop(...arr) {
   // using loop
-//   let num = 0;
-//   arr.forEach((n) => {
-//     num += n;
-//   });
-//   return num;
-
+  let num = 0;
+  arr.forEach((n) => {
+    num += n;
+  });
+  return num;
+}
+function sumReduce(...arr) {
   //using reduce
   // const num = arr.reduce((acc, b) => {
   //   return acc + b;
@@ -19,4 +20,5 @@ function sum(...arr) {
   // reduce - single line
   return arr.reduce((acc, b) => acc + b);
 }
-console.log("sum", sum(2, 3, 4, 5));
+console.log("sum", sumLoop(2, 3, 4, 5));
+console.log("sum", sumReduce(2, 3, 4, 5));
