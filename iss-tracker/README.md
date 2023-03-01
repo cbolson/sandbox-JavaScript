3c81cbd754acf0c2393bb4c3d9b72d421ad61bc0
cbolson <chris@cbolson.com>
2023-03-01 21:05:21 +0100

    new project - ISS tracker

---
+++ iss-tracker/README.md
@@ -0,0 +1,12 @@
+# ISS tracker
+
+## Description
+
+JSON based ISS tracker
+
+## Sources:
+
+- Data: https://wheretheiss.at/
+- Tutorial: https://www.youtube.com/watch?v=uxf0--uiX0I&t=334s
+- https://leafletjs.com
+- icon: <a href="https://www.flaticon.com/free-icons/satelite" title="satelite icons">Satelite icons created by Prosymbols Premium - Flaticon</a>
---
+++ iss-tracker/main.js
@@ -0,0 +1,38 @@
+//const urlISS  = 'https://api.wheretheiss.at/v1/satellites';
+const urlISS = "https://api.wheretheiss.at/v1/satellites/25544";
+const mapZoom = 3;
+
+// Leaflet
+const map = L.map("map").setView([0, 0], mapZoom); // "L" = Leaflet
+
+// OpenStreetMap tiles
+const attribution =
+  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileUrl = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+
+// add tiles to map
+tiles.addTo(map);
+
+// map marker
+const iconISS = L.icon({
+  iconUrl: "./assets/satellite.png",
+  iconSize: [64, 64],
+  iconAnchor: [32, 32], // center
+});
+const marker = L.marker([0, 0], { icon: iconISS }).addTo(map);
+
+// load iss position
+async function getISS() {
+  const response = await fetch(urlISS);
+  const data = await response.json();
+  addToMap(data);
+}
+
+function addToMap(data) {
+  const { latitude, longitude } = data;
+  marker.setLatLng([latitude, longitude]);
+  map.setView([latitude, longitude], mapZoom);
+}
+getISS();
+setInterval(getISS, 5000);
