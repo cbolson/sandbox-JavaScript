09611b20c4eeb96dbf41cfb279a77942d292b577
cbolson <chris@cbolson.com>
2023-05-15 21:17:04 +0200

    update with compressed images

--- rotating-globe/index.js
+++ rotating-globe/index.js
@@ -6,10 +6,9 @@ let moveVal = 1;
 window.addEventListener("scroll", () => {
   let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
   moveVal = Math.floor((scrollTop / height) * 359);
+  // patch as I don't have an image for 0
+  if (moveVal == 0) moveVal = 1;
   const numPic = moveVal.toString().padStart(3, "0");
   //console.log(moveVal);
-  globe.setAttribute(
-    "src",
-    `./globe/frame_${numPic}_delay-0.01s.gif`
-  );
+  globe.setAttribute("src", `./globe/frame_${numPic}_delay-0.01s.gif`);
 });
