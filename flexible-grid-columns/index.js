ba4b42a55b3e6da10ee4a17fff4a47415126545b
cbolson <chris@cbolson.com>
2023-05-19 21:36:02 +0200

    update - animation on bottom cell

--- flexible-grid-columns/index.js
+++ flexible-grid-columns/index.js
@@ -3,14 +3,21 @@ const boxes = document.querySelectorAll(".box");
 boxes.forEach((thisBox) => {
   const otherBox = boxes[thisBox.getAttribute("data-rel") - 1];
   const btn = thisBox.querySelector("button");
-
+    let isOpen = false;
   btn.addEventListener("click", () => {
     //remove expand and hidden classes on all boxes
     // boxes.forEach((box) => {
     //   box.classList.remove("hidden", "expand");
     // });
-
-    thisBox.classList.add("overlap","expand");
-    otherBox.classList.add("overlap", "hidden");
+console.log(isOpen);
+    if (!isOpen){
+        thisBox.classList.add("overlap", "expand");
+        otherBox.classList.add("overlap", "hidden");
+        isOpen = true;
+    }else{
+        thisBox.classList.remove("overlap", "expand");
+        otherBox.classList.remove("overlap", "hidden");
+        isOpen = false;
+    }
   });
 });
